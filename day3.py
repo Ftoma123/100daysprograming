cac9ae30db573571dcb00548a0ac327b922e0216
Ftoma123 <fatima.almulhim@hotmail.com>
2019-08-27 22:02:54 +0300

    first commit

---
+++ day3.py
@@ -0,0 +1,55 @@
+#example 1
+x = 5
+y = 'jhon1'
+print(x)
+print(y)
+print('_______')
+
+#example 2
+x = 4
+x = 'Sally'
+print(x)
+print('_______')
+
+#example 3
+x = 'Jhon'
+y = "Jhon"
+print(x)
+print(y)
+print('_______')
+
+#example 4
+x, y, z = "Banana", "Orange", "Apple"
+print(x)
+print(y)
+print(z)
+print('_______')
+
+#example 5
+x = y =z = "Orange"
+print(x)
+print(y)
+print(z)
+print('_______')
+
+#example 6
+x = 'Awesome'
+print('Python is '+x)
+print('_______')
+
+#example 7
+x = "Python "
+y = "is Awesome"
+print(x + y)
+print('_______')
+
+#example 8
+x = 5
+y = 4
+print(x+y)
+print('_______')
+
+#example 9
+x = 5
+y = 'jhon'
+print(x + y)
