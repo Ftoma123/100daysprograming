6a95142002bb9a006232cfe369ad06401abbe679
Ftoma123 <fatima.almulhim@hotmail.com>
2019-08-29 17:27:36 +0300

    Day 6

---
+++ day6.py
@@ -0,0 +1,24 @@
+#type casting
+x = 13
+y = 1.68
+z = "1"
+
+#int()
+print("Int() casting")
+print(int(x))
+print(int(y))
+print(int(z))
+
+#float()
+print("Float() casting")
+print(float(x))
+print(float(y))
+print(float(z))
+
+#str()
+print("Str() casting")
+print(str(x))
+print(str(y))
+print(str(z))
+
+
