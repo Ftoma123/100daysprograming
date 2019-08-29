4769fbb721ecde4c64f62aac834e40be3c071463
Ftoma123 <fatima.almulhim@hotmail.com>
2019-08-30 02:19:18 +0300

    Day 11

---
+++ day11.py
@@ -0,0 +1,18 @@
+# Python Logical Operators 
+x = 5
+print(x > 2 or x <8)
+
+
+#  Python Identity Operators 
+x = ["banana", "apple"]
+y = ["banana", "apple"]
+z = y
+
+print(x is not y)
+print(x is not y)
+print(x !=y)
+
+
+#Python Membership Operators
+x = ["banana", "apple"]
+print("apple" not in x)
