ee7f336e563bb15ae885b92db9cc5bdaa32250fd
Ftoma123 <fatima.almulhim@hotmail.com>
2019-08-30 01:29:09 +0300

    Day 8

---
+++ day8.py
@@ -0,0 +1,19 @@
+a = "Hello, World"
+
+#remove begining and ending whitespace
+print(a.strip())
+
+#length of string
+print(len(a))
+
+#lowercase
+print(a.lower())
+
+#upercase
+print(a.upper())
+
+#replace
+print(a.replace('l','f'))
+
+#split world
+print(a.split(','))
