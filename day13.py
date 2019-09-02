7ce9adf6574a65ca98683c1c172a14df4a86c0ed
Ftoma123 <fatima.almulhim@hotmail.com>
2019-09-02 22:02:17 +0300

    Day 13

---
+++ day13.py
@@ -0,0 +1,27 @@
+# Printing List []
+list = [10, 20, 30]
+print(list)
+
+# Access Items 
+list1 = ["apple", "banana", 'Grape']
+print(list1[2])
+
+# Loop Through a List 
+list2 = [1, 2, 3, 4, 10, 20, 30]
+for x in list2:
+    print(x)
+
+# Change Item Value
+list3 = ["apple", "banana", 'Grape']
+list3[1] = "Strawberry"
+print(list3)
+
+# Remove Item Value
+list4 = ["apple", "banana", 'Grape']
+del list4[1]
+print(list4)
+
+#Remove whole list
+list5 = ["apple", "banana", 'Grape']
+del list5
+print(list5)
