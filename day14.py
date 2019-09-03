67343d64e972f8b28935a5e38f25b22b2bff5a11
Ftoma123 <fatima.almulhim@hotmail.com>
2019-09-03 22:45:22 +0300

    Day 14

---
+++ day14.py
@@ -0,0 +1,17 @@
+# print part of the list
+list1 = [1, 2, 3, 4, 5, 6, 7]
+print(list1[1:4])
+
+# Check if Item Exists
+list2 = ['apple','banana','mango','dates']
+if 'mango' in list2:
+    print('its in the list')
+
+# Repeat Item in List
+list3 = ['Fatima']*4
+print(list3)
+
+# + Operator in List
+list4 = [1,2,3,4,5,6]
+list5 = [10, 20, 30, 40, 50, 60]
+print(list4 + list5)
