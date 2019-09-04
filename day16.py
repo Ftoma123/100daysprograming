995f93a722df63accc6165d4f222063282489927
Ftoma123 <fatima.almulhim@hotmail.com>
2019-09-04 22:09:13 +0300

    Day 16

---
+++ day16.py
@@ -0,0 +1,29 @@
+# Create a Tuple
+tuple1 = ('A','B','C')
+print(tuple1)
+
+# empty tuple and tuble with one value
+tuple2 = ()
+tuple3 = ('A',)
+print(tuple2)
+print(tuple3)
+
+# Access tuple item
+tuple4 = ('Apple','Banana','Cat')
+print(tuple4[1])
+
+# loop in tuple
+tuple5 = ('Apple','Banana','Cat')
+for x in tuple5:
+    print(x)
+
+
+# print part of tuple
+tuple7 = ('Apple','Banana','Cat')
+print(tuple7[0:1])
+
+# del() to delete tuple completly 
+tuple6 = ('Apple','Banana','Cat')
+del tuple6
+print(tuple6) # error
+
