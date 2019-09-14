675f8fabd74651dac40cf765dffb3898d5ad1f76
Ftoma123 <fatima.almulhim@hotmail.com>
2019-09-14 23:03:35 +0300

    Day 18 19

---
+++ day19_18.py
@@ -0,0 +1,19 @@
+#Fits question:
+
+list1 = ['Apple', 'cat', 'banana', 'bed']
+#add element at the end
+list1.append('Red')
+#add element at specific position
+list1.insert(1,'Green')
+#remove element from specific position
+list1.pop(0)
+#reverse the order of the list
+list1.reverse()
+print(list1)
+
+
+#Second question:
+  
+tuple1 = ('java', 'python', 'swift')
+if 'python' in tuple1:
+    print('True, Python is in the list') 
