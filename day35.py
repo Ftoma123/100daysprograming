04fdfe58fd3fa66935e5b4f3ebf156f0f8f27097
Ftoma123 <fatima.almulhim@hotmail.com>
2019-10-03 21:57:58 +0300

    Day 35

---
+++ day35.py
@@ -0,0 +1,14 @@
+#lambda function
+x = lambda a:a + 10
+print(x(4))
+print('_________')
+
+#lambda with multi arguments
+z = lambda a, b : a*b
+print(z(3,8))
+print('_________')
+
+
+#lambda with multi arguments
+y = lambda a,b,c : a + b + c
+print(y(2,3,4))
