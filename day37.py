2ea690063498a2e21e02804f0369a5d16f98208f
Ftoma123 <fatima.almulhim@hotmail.com>
2019-10-03 21:59:48 +0300

    Day 27

---
+++ day37.py
@@ -0,0 +1,15 @@
+#Array
+cars = ['Ford', 'Ferari', 'GMC']
+print(cars)
+
+#access element in array
+x = cars[2]
+print(x)
+
+#modify element in array
+cars[0] = 'BMW'
+print(cars)
+
+#length of array
+array_len = len(cars)
+print(array_len)
